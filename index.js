4c8fccb7af4f0b6dc26fd82c5689fb6cdb99d31c
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 16:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const mondayWork = function(activity = "go to the office") {
 
 function wrapAdjective(string1 = '*') {
   return function(string2 = "special") {
-    return `You are ${string2}`;
+    return `You are ${string1}${string2}${string1}`;
   };
 }
